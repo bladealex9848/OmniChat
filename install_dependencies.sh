bf1056b28e44a0418142f1d950dcf3237085af3a
Alexander Oviedo Fadul <bladealex@gmail.com>
2024-08-05 16:14:37 -0500

    Actualización de requerimientos

---
+++ install_dependencies.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+pip install openai==1.39.0 SQLAlchemy==2.0.32 pymysql==1.1.1 streamlit==1.37.1
+pip install duckduckgo-search==6.2.6 pypdf==4.3.1
+pip install langchain-core
+pip install langchain
+pip install langchain-community langchain-openai
+pip install faiss-cpu==1.7.4 sentence-transformers==2.3.1 fastembed==0.3.4 validators==0.22.0
